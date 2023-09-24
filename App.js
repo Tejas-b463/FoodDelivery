725006c26dcdbe4fd3a6dbf8c114d3bcc75c10ec
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-09-24 14:36:25 +0530

    install parcel, react and react dom

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", { id: "heading" }, "Food Delivery")
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
