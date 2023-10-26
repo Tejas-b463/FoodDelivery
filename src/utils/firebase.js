f97ed51e42fc94054e1812745e4dfeebfbe1a06a
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-10-26 22:31:11 +0530

    Bug Fix

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
 
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-        apiKey: "AIzaSyCTwHJUW12L5fQw2Q87pEKl_9iem5SB0Jo",
-        authDomain: "instanteats-7a680.firebaseapp.com",
-        projectId: "instanteats-7a680",
-        storageBucket: "instanteats-7a680.appspot.com",
-        messagingSenderId: "456754000910",
-        appId: "1:456754000910:web:3a9697c68a0d2eba32385f",
-        measurementId: "G-WPJK03MT5Q"
-    }
-    // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
+    apiKey: "AIzaSyCTwHJUW12L5fQw2Q87pEKl_9iem5SB0Jo",
+    authDomain: "instanteats-7a680.firebaseapp.com",
+    projectId: "instanteats-7a680",
+    storageBucket: "instanteats-7a680.appspot.com",
+    messagingSenderId: "456754000910",
+    appId: "1:456754000910:web:3a9697c68a0d2eba32385f",
+    measurementId: "G-WPJK03MT5Q",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
